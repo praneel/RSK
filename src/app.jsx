6f5123bd02516773b47e5b36c3e98f7901cdafa7
Praneel PIDIKITI <pidikiti@live.com>
2015-07-10 15:03:09 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+
+var React = require("react");
+
+var App=React.createClass({
+
+    render()
+    {
+      return <h1>Hello</h1>;
+
+
+    }
+
+
+  })
+
+React.render(<App/>, document.getelementbyId('example'));
